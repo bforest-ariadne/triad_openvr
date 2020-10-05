0e3a5554890d306c4edd426a7ba1fc5aa9d7b2a4
Ben Forest <ben@ben-forest.com>
2020-10-05 15:54:58 -0400

    update remap script

--- bs_rmap.py
+++ bs_rmap.py
@@ -1,17 +1,29 @@
-in1_sides = [round(chan.vals[0], 2) for chan in op('all_sides').chans('in1*')]
-in2_sides = [round(chan.vals[0], 2) for chan in op('all_sides').chans('in2*')]
-in3_sides = [round(chan.vals[0], 2) for chan in op('all_sides').chans('in3*')]
+def remap_bs():
+    in1_sides = [round(chan.vals[0], 2) for chan in op('all_sides').chans('in1*')]
+    in2_sides = [round(chan.vals[0], 2) for chan in op('all_sides').chans('in2*')]
+    in3_sides = [round(chan.vals[0], 2) for chan in op('all_sides').chans('in3*')]
 
-in_sides_list = [in1_sides, in2_sides, in3_sides]
+    in_sides_list = [in1_sides, in2_sides, in3_sides]
 
-ref_sides = [round(float(side.val), 2) for side in op('sides').col(0)]
+    ref_sides = remove_dupes([round(chan.vals[0], 2) for chan in op('all_sides').chans('*')])
+    ref_sides.sort()
 
-# print('ref_sides', ref_sides)
+    print('ref_sides', ref_sides)
 
-for idx, in_sides in enumerate(in_sides_list):
-    if ref_sides[0] in in_sides and ref_sides[2] in in_sides: 
-        op('bs1').par.chop.val = f"bs_in_{idx + 1}"
-    elif ref_sides[0] in in_sides and ref_sides[1] in in_sides:
-        op('bs2').par.chop.val = f"bs_in_{idx + 1}"
-    elif ref_sides[1] in in_sides and ref_sides[2] in in_sides:
-        op('bs3').par.chop.val = f"bs_in_{idx + 1}"
+    for idx, in_sides in enumerate(in_sides_list):
+        if ref_sides[0] in in_sides and ref_sides[2] in in_sides: 
+            op('bs1').par.chop.val = f"in{idx + 1}"
+        elif ref_sides[0] in in_sides and ref_sides[1] in in_sides:
+            op('bs2').par.chop.val = f"in{idx + 1}"
+        elif ref_sides[1] in in_sides and ref_sides[2] in in_sides:
+            op('bs3').par.chop.val = f"in{idx + 1}"
+
+def remove_dupes(list_in):
+    res = []
+    [res.append(x) for x in list_in if x not in res] 
+    return res
+
+
+def onValueChange(channel, sampleIndex, val, prev):
+    remap_bs()
+    return
